2c8bc734eb9d88d87b43e08ac7581c4b6d644db1
ED <carteroffice@gmail.com>
2018-10-09 11:30:10 -0400

    testing checkout etc.

--- src/lab2/Lab2AreaPeriVoluApp.java
+++ src/lab2/Lab2AreaPeriVoluApp.java
@@ -77,4 +77,5 @@ public class Lab2AreaPeriVoluApp {
 	}
 	
 	//here's a change for git practice.
+	//here's another change for git practice.
 }
--- src/lab2/SavedByTheBell.java
+++
@@ -1,9 +0,0 @@
-package lab2;
-
-public class SavedByTheBell {
-
-	public static void main(String[] args) {
-		System.out.println("I'm a placeholder file to help practice and learn git!");
-	}
-
-}
